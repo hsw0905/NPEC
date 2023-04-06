3da7cce8f31b2a77173b60708ed04bbba5c08977
hautblis <hautblis@gmail.com>
2023-04-06 13:33:11 +0900

    프로젝트 세팅
    (cherry picked from commit 5682b5406a39b964ca8727fa0f3873d735b19ee3)

---
+++ src/main/java/com/mogak/npec/NpecApplication.java
@@ -0,0 +1,13 @@
+package com.mogak.npec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpecApplication.class, args);
+	}
+
+}
