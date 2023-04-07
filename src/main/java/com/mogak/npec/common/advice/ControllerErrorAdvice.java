a0290858f1778e3c370ec3eec99f13e53c793dd4
hsw0905 <hsw0905@gmail.com>
2023-04-07 12:05:47 +0900

    ExceptionAdvice 추가

--- src/main/java/com/mogak/npec/common/advice/ControllerErrorAdvice.java
+++ src/main/java/com/mogak/npec/common/advice/ControllerErrorAdvice.java
@@ -1,7 +1,9 @@
 package com.mogak.npec.common.advice;
 
 import com.mogak.npec.common.dto.ErrorResponse;
+import com.mogak.npec.common.exception.BadRequestException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,7 +11,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ControllerErrorAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleBadRequestException(MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorResponse> handleMethodArgumentException(BindingResult bindingResult) {
+        String errorMessage = bindingResult.getFieldErrors()
+                .get(0)
+                .getDefaultMessage();
+        return ResponseEntity.badRequest().body(new ErrorResponse(errorMessage));
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException e) {
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity.internalServerError().body(new ErrorResponse("서버 내부에 문제가 발생했습니다."));
     }
 }
