7e7e9f38a444cc1b2067588d1a9b43cf80a02648
hautblis <hautblis@gmail.com>
2023-04-18 18:04:26 +0900

    logout() , refresh() 테스트 추가

--- src/main/java/com/mogak/npec/auth/domain/BlackList.java
+++ src/main/java/com/mogak/npec/auth/domain/BlackList.java
@@ -1,10 +1,12 @@
 package com.mogak.npec.auth.domain;
 
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
 @RedisHash
+@Getter
 public class BlackList {
     @Id
     private String token;
--- src/test/java/com/mogak/npec/auth/application/AuthServiceTest.java
+++ src/test/java/com/mogak/npec/auth/application/AuthServiceTest.java
@@ -1,10 +1,11 @@
 package com.mogak.npec.auth.application;
 
 import com.mogak.npec.auth.domain.EncryptorImpl;
-import com.mogak.npec.auth.application.AuthService;
 import com.mogak.npec.auth.domain.TokenProvider;
 import com.mogak.npec.auth.dto.LoginRequest;
 import com.mogak.npec.auth.dto.LoginTokenResponse;
+import com.mogak.npec.auth.dto.RefreshResponse;
+import com.mogak.npec.auth.exception.InvalidTokenException;
 import com.mogak.npec.auth.exception.LoginFailException;
 import com.mogak.npec.auth.repository.BlackListRepository;
 import com.mogak.npec.member.domain.Member;
@@ -20,6 +21,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 class AuthServiceTest {
+    private final String EXPIRED_TOKEN = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MSwiaWF0IjoxNjgxODAwNDgzLCJleHAiOjE2ODE4MDA1NDN9.Z4vT9gS2I-dT7ljOfPJmtQBWjl5T7806IycG0xRwk58";
+    private final String TOKEN_PREFIX = "Bearer ";
+
     @Autowired
     private TokenProvider tokenProvider;
     @Autowired
@@ -67,12 +71,37 @@ class AuthServiceTest {
     @Test
     void logoutSuccess() {
         // given
-        String tokenPrefix = "Bearer ";
         String accessToken = tokenProvider.createAccessToken(1L);
         String refreshToken = tokenProvider.createRefreshToken(1L);
 
         // when
-        authService.logout(tokenPrefix + accessToken, tokenPrefix + refreshToken);
+        authService.logout(TOKEN_PREFIX + accessToken, TOKEN_PREFIX + refreshToken);
 
+        // then
+        assertThat(blackListRepository.findAll().size()).isEqualTo(2);
+    }
+
+    @DisplayName("만료된 토큰으로 로그아웃 요청시 예외를 던진다")
+    @Test
+    void logoutFailWithExpiredToken() {
+        String refreshToken = tokenProvider.createRefreshToken(1L);
+
+        assertThatThrownBy(
+                () -> authService.logout(EXPIRED_TOKEN, TOKEN_PREFIX + refreshToken)
+        ).isExactlyInstanceOf(InvalidTokenException.class);
+    }
+
+    @Test
+    void refreshSuccess() {
+        // given
+        String refreshToken = tokenProvider.createRefreshToken(1L);
+
+        // when
+        RefreshResponse response = authService.refresh(TOKEN_PREFIX + refreshToken);
+
+        // then
+        assertThat(response.getAccessToken()).isNotEmpty();
+        assertThat(response.getRefreshToken()).isNotEmpty();
+        assertThat(blackListRepository.findById(refreshToken)).isNotEmpty();
     }
 }
