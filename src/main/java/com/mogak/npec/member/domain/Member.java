3fad69f033c7206c502bde3e5cdb262466edc0e8
hsw0905 <hsw0905@gmail.com>
2023-05-17 11:56:29 +0900

    bulk insert Member

--- src/main/java/com/mogak/npec/member/domain/Member.java
+++ src/main/java/com/mogak/npec/member/domain/Member.java
@@ -11,7 +11,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
+
 @Entity
 @Table(name = "members")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -30,9 +30,35 @@ public class Member extends BaseEntity {
     @Column(length = 100)
     private String password;
 
+    @Column(name = "is_out")
     private boolean isOut;
 
+    @Column(name = "image_url", length = 300)
     private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean getIsOut() {
+        return isOut;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
 
     public Member(String nickname, String email, String password) {
         this.nickname = nickname;
@@ -40,10 +66,10 @@ public class Member extends BaseEntity {
         this.password = password;
     }
 
+
     public boolean match(Member targetMember) {
         return this.equals(targetMember);
     }
-
 
     public void changeNickname(String nickname) {
         this.nickname = nickname;
