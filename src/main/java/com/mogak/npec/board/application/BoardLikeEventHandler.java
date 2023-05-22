66f8ebaccd6312d7a51c51af45f55880c9e1c1c4
hautblis <hautblis@gmail.com>
2023-05-22 21:22:31 +0900

    좋아요 정합성 기능 리팩토링

--- src/main/java/com/mogak/npec/board/application/BoardLikeEventHandler.java
+++ src/main/java/com/mogak/npec/board/application/BoardLikeEventHandler.java
@@ -2,26 +2,27 @@ package com.mogak.npec.board.application;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.context.event.EventListener;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class BoardLikeEventHandler {
     private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper mapper;
 
-    public BoardLikeEventHandler(RedisTemplate<String, Object> redisTemplate) {
+    public BoardLikeEventHandler(RedisTemplate<String, Object> redisTemplate, ObjectMapper mapper) {
         this.redisTemplate = redisTemplate;
+        this.mapper = mapper;
     }
 
-    @EventListener
+    @TransactionalEventListener
     public void increaseBoardLikeCount(BoardLikeCreatedEvent event) throws JsonProcessingException {
         SetOperations<String, Object> setOps = redisTemplate.opsForSet();
         BoardLikeModifiedMessage message = new BoardLikeModifiedMessage(event.getBoardId());
-
-        ObjectMapper mapper = new ObjectMapper();
         String jsonString = mapper.writeValueAsString(message);
+
         setOps.add("boardLike", jsonString);
     }
 }
--- src/main/java/com/mogak/npec/board/application/ScheduledService.java
+++ src/main/java/com/mogak/npec/board/application/ScheduledService.java
@@ -2,12 +2,11 @@ package com.mogak.npec.board.application;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mogak.npec.board.repository.BoardLikeRepository;
-import com.mogak.npec.board.repository.BoardSortRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -15,41 +14,38 @@ import java.util.List;
 @Slf4j
 public class ScheduledService {
     private final RedisTemplate<String, Object> redisTemplate;
-    private final BoardSortRepository boardSortRepository;
-    private final BoardLikeRepository boardLikeRepository;
+    private final BoardLikeCountSyncService boardLikeCountSyncService;
+    private final ObjectMapper mapper;
 
-    public ScheduledService(RedisTemplate<String, Object> redisTemplate, BoardSortRepository boardSortRepository, BoardLikeRepository boardLikeRepository) {
+    public ScheduledService(RedisTemplate<String, Object> redisTemplate, BoardLikeCountSyncService boardLikeCountSyncService, ObjectMapper mapper) {
         this.redisTemplate = redisTemplate;
-        this.boardSortRepository = boardSortRepository;
-        this.boardLikeRepository = boardLikeRepository;
+        this.boardLikeCountSyncService = boardLikeCountSyncService;
+        this.mapper = mapper;
     }
 
     @Scheduled(fixedDelay = 10000)
     public void run() {
-        List<Object> boardLikeMessagesForString = redisTemplate.opsForSet().pop("boardLike", 100);
-        ObjectMapper mapper = new ObjectMapper();
+        List<Object> boardLikeEvents = redisTemplate.opsForSet().pop("boardLike", 100);
 
-        assert boardLikeMessagesForString != null;
-        if (boardLikeMessagesForString.isEmpty()) {
+        if (CollectionUtils.isEmpty(boardLikeEvents)) {
             return;
         }
 
-        List<BoardLikeModifiedMessage> messages = boardLikeMessagesForString.stream()
-                .map(it -> {
+        List<BoardLikeModifiedMessage> messages = convertMessageFromEvent(boardLikeEvents);
+
+        boardLikeCountSyncService.updateBoardSortsLikeCount(messages);
+    }
+
+    private List<BoardLikeModifiedMessage> convertMessageFromEvent(List<Object> messages) {
+        return messages.stream()
+                .map(message -> {
                     try {
-                        return mapper.readValue((String) it, BoardLikeModifiedMessage.class);
+                        return mapper.readValue((String) message, BoardLikeModifiedMessage.class);
                     } catch (JsonProcessingException e) {
-                        log.error("BoardLikeModifiedMessage.class 로 변환 에러");
-                        throw new RuntimeException();
+                        log.error("BoardLikeModifiedMessage.class 로 변환 에러. value: {}", message);
+                        throw new RuntimeException(e);
                     }
                 }).toList();
-
-        messages.forEach(
-                message -> {
-                    Long boardId = message.getBoardId();
-                    Long count = boardLikeRepository.countByBoardId(boardId);
-                    boardSortRepository.updateLikeCount(count, boardId);
-                }
-        );
     }
+
 }
