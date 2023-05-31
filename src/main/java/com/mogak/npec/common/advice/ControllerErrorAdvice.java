60fcb7529250911b43def32f1f05e25f322e1e13
hsw0905 <hsw0905@gmail.com>
2023-05-31 14:08:37 +0900

    error log 추가

--- src/main/java/com/mogak/npec/common/advice/ControllerErrorAdvice.java
+++ src/main/java/com/mogak/npec/common/advice/ControllerErrorAdvice.java
@@ -4,6 +4,7 @@ import com.mogak.npec.common.dto.ErrorResponse;
 import com.mogak.npec.common.exception.BadRequestException;
 import com.mogak.npec.common.exception.NotFoundException;
 import com.mogak.npec.common.exception.UnAuthorizedException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+@Slf4j
 @RestControllerAdvice
 public class ControllerErrorAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -19,34 +21,41 @@ public class ControllerErrorAdvice {
         String errorMessage = bindingResult.getFieldErrors()
                 .get(0)
                 .getDefaultMessage();
+        log.error(errorMessage);
         return ResponseEntity.badRequest().body(new ErrorResponse(errorMessage));
     }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException e) {
+        log.error(e.getMessage());
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
+        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(UnAuthorizedException.class)
     public ResponseEntity<ErrorResponse> handleUnAuthorizedException(UnAuthorizedException e) {
+        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        log.error(e.getMessage());
         return ResponseEntity.internalServerError().body(new ErrorResponse("서버 내부에 문제가 발생했습니다."));
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorResponse> handleTypeMismatchException(MethodArgumentTypeMismatchException e) {
         if (e.getName().equals("sort")) {
+            log.error("유효하지않은 sort 타입입니다.");
             return ResponseEntity.badRequest().body(new ErrorResponse("유효하지않은 sort 타입입니다."));
         }
+        log.error(e.getMessage());
         return ResponseEntity.internalServerError().body(new ErrorResponse(e.getMessage()));
     }
 }
